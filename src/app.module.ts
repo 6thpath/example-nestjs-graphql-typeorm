4acdf28dc6473890f228463112fe6a856ef61e8b
Paths <6thPath>
2018-12-20 14:39:46 +0700

    add files

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { CatsModule } from './cats/cats.module'
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
+        outputAs: 'class'
+      },
+      playground: {
+        settings: {
+          'general.betaUpdates': false,
+          'editor.cursorShape': 'line', // possible values: 'line', 'block', 'underline'
+          'editor.fontSize': 14,
+          'editor.fontFamily': `'Source Code Pro', 'Consolas', 'Inconsolata', 'Droid Sans Mono', 'Monaco', monospace`,
+          'editor.theme': 'dark', // possible values: 'dark', 'light'
+          'editor.reuseHeaders': true, // new tab reuses headers from last tab
+          'request.credentials': 'omit', // possible values: 'omit', 'include', 'same-origin'
+          'tracing.hideTracingResponse': true
+        }
+      }
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class ApplicationModule {}
