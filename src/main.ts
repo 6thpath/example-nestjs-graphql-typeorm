ca86c8fc84e1b5cb29fd12079bafca63a7aa16aa
Phat Mai <tienphat1501@gmail.com>
2019-08-12 07:05:29 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,17 @@
 import * as env from 'dotenv'
 env.config()
 import * as cors from 'cors'
-import { NestFactory, FastifyAdapter } from '@nestjs/core'
+import { NestFactory } from '@nestjs/core'
 import { ApplicationModule } from './modules/app/module'
 // import { MyLogger } from './common/logger/LoggerService'
 
 async function bootstrap() {
   const app = await NestFactory.create(
-    ApplicationModule,
-    new FastifyAdapter(),
+    ApplicationModule
     // { logger: new MyLogger() }
   )
   app.use(cors())
-  await app.listen(process.env.PORT || 3000, '0.0.0.0')
+  await app.listen(process.env.PORT || 3000)
 }
 
 bootstrap()
--- src/schema/cats.schema.ts
+++ src/schema/cats.schema.ts
@@ -1,39 +1,45 @@
-import { ApolloError } from 'apollo-server-express'
+import { ApolloError } from 'apollo-server-express';
 
 export class CreateCatInput {
-  name?: string
-  age?: number
+  name?: string;
+  age?: number;
 }
 
 export class UpdateCatInput {
-  _id: string
-  name?: string
-  age?: number
+  _id: string;
+  name?: string;
+  age?: number;
 }
 
 export class Cat {
-  _id?: string
-  name?: string
-  age?: number
+  _id?: string;
+  name?: string;
+  age?: number;
 
   constructor(args: any) {
-    this._id = args._id
-    this.name = args.name
-    this.age = args.age
+    this._id = args._id;
+    this.name = args.name;
+    this.age = args.age;
   }
 }
 
 export abstract class IQuery {
-  abstract getCats(): Cat[] | Promise<Cat[]|ApolloError> | ApolloError
-  abstract getCat(_id: string): Cat | Promise<Cat|ApolloError> | ApolloError
+  abstract getCats(): Cat[] | Promise<Cat[] | ApolloError> | ApolloError;
+  abstract getCat(_id: string): Cat | Promise<Cat | ApolloError> | ApolloError;
 }
 
 export abstract class IMutation {
-  abstract createCat(createCatInput?: CreateCatInput): Cat | Promise<Cat|ApolloError> | ApolloError
-  abstract updateCat(updateCatInput: UpdateCatInput): Cat | Promise<Cat|ApolloError> | ApolloError
-  abstract deleteCat(_id: string): Cat | Promise<Cat|ApolloError> | ApolloError
+  abstract createCat(
+    createCatInput?: CreateCatInput,
+  ): Cat | Promise<Cat | ApolloError> | ApolloError;
+  abstract updateCat(
+    updateCatInput: UpdateCatInput,
+  ): Cat | Promise<Cat | ApolloError> | ApolloError;
+  abstract deleteCat(
+    _id: string,
+  ): Cat | Promise<Cat | ApolloError> | ApolloError;
 }
 
 export abstract class ISubscription {
-  abstract catEvent(): Cat | Promise<Cat|ApolloError> | ApolloError
+  abstract catEvent(): Cat | Promise<Cat | ApolloError> | ApolloError;
 }
